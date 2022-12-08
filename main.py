a8f77d8548b7f8d9b82a32d7da11e000ee0ea597
Snorlax <maria.vallejo29@yahoo.com>
2022-12-08 15:37:36 -0600

    no empty fields and ask if entered info is ok

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    with open("data.txt", "a") as file:
-        file.write(f"{website_input.get()} | {email_username_input.get()} | {password_input.get()}\n")
-    website_input.delete(0, END)
-    password_input.delete(0, END)
+
+    website = website_input.get()
+    email = email_username_input.get()
+    password = password_input.get()
+
+    if website == "" or password == "":
+        messagebox.askokcancel(title="Oops", message="Don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n Email: {email}"
+                                       f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+            website_input.delete(0, END)
+            password_input.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
